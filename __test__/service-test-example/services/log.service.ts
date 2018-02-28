3468bcb79c046d32357d3f44f1e46f07ada4cd29
axetroy <troy450409405@gmail.com>
2018-03-01 02:59:22 +0800

    update

--- __test__/service-test-example/services/log.service.ts
+++ __test__/service-test-example/services/log.service.ts
@@ -3,14 +3,7 @@ import { Service } from "../../../index";
 export default class LogService extends Service {
   level = 100; // log service should be init first
   public initedAt: Date;
-  public username: string;
-  async getUser() {
-    return {
-      name: "Axetroy"
-    };
-  }
   async init() {
-    this.username = "admin";
     this.initedAt = new Date();
 
     // do some job
